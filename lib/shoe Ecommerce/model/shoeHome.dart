dd8f0285d3211830f74747014e9e7b1befaea1d7
sreedeviluminar <sreedevi@luminartechnolab.com>
2024-04-24 09:58:08 +0530

    commit

--- lib/shoe Ecommerce/model/shoeHome.dart
+++ lib/shoe Ecommerce/model/shoeHome.dart
@@ -10,18 +10,36 @@ List<Shoe> shoelist = [
   Shoe(
     id: 1,
     image: "https://www.campusshoes.com/cdn/shop/products/FIRST_11G-787_L.GRY-BLK.jpg?v=1705644702",
-    name: "Diabetic SportsShoe",
+    name: "Diabetic SportsShoe 1",
     description: "Buy sports shoes, slippers, clogs, sneakers, formal shoes from Nike, Sparks, Red chief, Crocs and more at Amazon.in.",
   ),
   Shoe(
       id: 2,
       image: "https://m.media-amazon.com/images/I/61+hrj2FI5L._AC_UY1000_.jpg",
-      name: "Diabetic SportsShoe",
+      name: "Diabetic SportsShoe 2",
       description: "Orthopedic & Diabetic Sports Shoes Breathable Soft Sole Ultra-Lightweight Running/Walking Sports Shoes for Men's 722 "),
   Shoe(
       id: 3,
       image: "https://www.campusshoes.com/cdn/shop/products/FIRST_11G-787_L.GRY-BLK.jpg?v=1705644702",
-      name: "Diabetic SportsShoe",
+      name: "Diabetic SportsShoe 3",
       description: "HEALTH FIT Orthopedic & Diabetic Sports Shoes Breathable Soft Sole Ultra-Lightweight Running/Walking Sports Shoes for Men's 721BK-6 Black at Amazon.in.",
       ),
+  Shoe(
+    id: 1,
+    image: "https://www.campusshoes.com/cdn/shop/products/FIRST_11G-787_L.GRY-BLK.jpg?v=1705644702",
+    name: "Diabetic SportsShoe 1",
+    description: "Buy sports shoes, slippers, clogs, sneakers, formal shoes from Nike, Sparks, Red chief, Crocs and more at Amazon.in.",
+  ),
+  Shoe(
+      id: 2,
+      image: "https://m.media-amazon.com/images/I/61+hrj2FI5L._AC_UY1000_.jpg",
+      name: "Diabetic SportsShoe 2",
+      description: "Orthopedic & Diabetic Sports Shoes Breathable Soft Sole Ultra-Lightweight Running/Walking Sports Shoes for Men's 722 "),
+  Shoe(
+    id: 3,
+    image: "https://www.campusshoes.com/cdn/shop/products/FIRST_11G-787_L.GRY-BLK.jpg?v=1705644702",
+    name: "Diabetic SportsShoe 3",
+    description: "HEALTH FIT Orthopedic & Diabetic Sports Shoes Breathable Soft Sole Ultra-Lightweight Running/Walking Sports Shoes for Men's 721BK-6 Black at Amazon.in.",
+  ),
 ];
+
--- lib/shoe Ecommerce/screens/views/shoedetails.dart
+++ lib/shoe Ecommerce/screens/views/shoedetails.dart
@@ -1,11 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutterfeb/utils/textThemes.dart';
 
 import '../../model/shoeHome.dart';
 
 class ShoeDetails extends StatelessWidget {
  late Shoe shoe;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    ///fetching the arguments passed from previous screen
+    final index = ModalRoute.of(context)?.settings.arguments as int;
+
+    /// setting the data from that particular index to shoe Object
+    shoe = shoelist[index];
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('${shoe.name} Details'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.network(shoe.image ?? ""),
+            Text(shoe.name ?? "", style: MyTextThemes.textHeading),
+            Text(
+              shoe.description ?? "",
+              style: MyTextThemes.bodyTextStyle,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
